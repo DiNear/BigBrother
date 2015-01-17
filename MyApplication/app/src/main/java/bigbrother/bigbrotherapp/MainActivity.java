7ba9c08f4498c09b7997ed6ccb333b74566db3e0
Tony Wang <twseco@hotmail.com>
2015-01-17 10:25:49 -0500

    changesgui

--- MyApplication/app/src/main/java/bigbrother/bigbrotherapp/MainActivity.java
+++ MyApplication/app/src/main/java/bigbrother/bigbrotherapp/MainActivity.java
@@ -2,18 +2,23 @@ package bigbrother.bigbrotherapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +41,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
