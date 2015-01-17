5cdacdfc0f527f851f0bd8b8f97808c39827cc06
Andrew Tan <at@andrewt.me>
2015-01-17 03:24:42 -0500

    Started REST api work

---
+++ MyApplication/app/src/androidTest/java/bigbrother/bigbrotherapp/Person.java
@@ -0,0 +1,40 @@
+package bigbrother.bigbrotherapp;
+
+
+public class Person {
+    private String firstname;
+    private String lastname;
+    private String last_check;
+    private String token;
+    private int check_freq;
+    private int pin;
+    private int id;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getLast_check() {
+        return last_check;
+    }
+
+    public String token() {
+        return token;
+    }
+
+    public int getCheck_freq() {
+        return check_freq;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
