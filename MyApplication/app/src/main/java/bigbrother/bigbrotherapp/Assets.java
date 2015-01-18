843b53f10b00c484051f66d7e9448ee6df7762f2
Tony Wang <twseco@hotmail.com>
2015-01-18 02:49:07 -0500

    lolol

---
+++ MyApplication/app/src/main/java/bigbrother/bigbrotherapp/Assets.java
@@ -0,0 +1,23 @@
+package bigbrother.bigbrotherapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+/**
+ * Created by Tony on 1/17/2015.
+ */
+public class Assets {
+    public static final String MyPreferences = "MyPrefs";
+    SharedPreferences sharedpreferences;
+    public Assets(){
+
+
+    }
+
+
+}
